7bad12eca588365d1856f47a603b01e89753fc9b
shaikkashu <159931158+shaikkashu@users.noreply.github.com>
2024-05-17 07:09:33 +0000

    change the two numbers to three numbers

---
+++ impe.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+int main()
+{
+    int num1,num2;
+    int result;
+    printf("enter two numbers:");
+    scanf("%d %d",&num1,&num2);
+    result=num1+num2;
+    printf("sum of %d and %d is %d",num1,num2,result);
+    return 0;
+}
